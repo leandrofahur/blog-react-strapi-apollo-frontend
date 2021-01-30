1402ef4a8f6b8806fe2df09b1213ff69e03ab6a9
leandro machado <fahurleandro@gmail.com>
2021-01-30 11:50:17 -0800

    frontend for the strapi backend api

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import App from "./components/App";
+import client from "./utils/apolloClient";
+
+ReactDOM.render(
+  <Router>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>,
+  document.getElementById("root")
+);
